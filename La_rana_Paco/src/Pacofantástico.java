da2c970dce35e94fc2958208ee9ef32c05773c4c
japaro22 <javi@prueba.com>
2016-05-23 13:21:11 +0100

    Paco cabroncete no te rralles tt

---
+++ La_rana_Paco/src/Pacofantástico.java
@@ -0,0 +1,11 @@
+
+public class Pacofantástico {
+
+	public static void main(String[] args) {
+		// Te tengo vigilado el coche
+		//como estos conflictos de los que hablas
+		//sean complicados, te lo dejo en 4 ladrillos
+
+	}
+
+}
