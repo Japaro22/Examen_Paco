60bbb4f8c2ae2c669a9a6c2902187796d704e95c
japaro22 <javi@prueba.com>
2016-05-23 13:39:50 +0100

    Javi matamoros desmiente su homosexualidad v2

--- La_rana_Paco/src/Pacofantástico.java
+++ La_rana_Paco/src/Pacofantástico.java
@@ -2,7 +2,7 @@
 public class Pacofantástico {
 
 	public static void main(String[] args) {
-		for(int i=0;i<10000;i++) System.out.println("TU ERES EL MAS PESADO De TODOS GAY");
+		for(int i=0;i<10000;i++) System.out.println("Te voy a denunciar por pedofilo caaca");
 
 	}
 
